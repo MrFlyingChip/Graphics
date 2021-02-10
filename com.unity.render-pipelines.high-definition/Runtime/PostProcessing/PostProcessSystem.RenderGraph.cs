aa964f6bd29bdec4f8ce022755aeae89e8b109dd
Kleber Garcia <kleber.garcia@unity3d.com>
2021-02-10 09:36:24 -0500

    Setting camera schedules during certain parts of the frame

--- com.unity.render-pipelines.high-definition/Runtime/PostProcessing/PostProcessSystem.RenderGraph.cs
+++ com.unity.render-pipelines.high-definition/Runtime/PostProcessing/PostProcessSystem.RenderGraph.cs
@@ -1089,6 +1089,8 @@ namespace UnityEngine.Rendering.HighDefinition
                     motionVectors = upsamplignSceneResults.motionVectors;
                 }
 
+                hdCamera.Schedule = HDCamera.CameraSchedule.PostProcessing;
+
                 source = CustomPostProcessPass(renderGraph, hdCamera, source, depthBuffer, normalBuffer, HDRenderPipeline.defaultAsset.beforeTAACustomPostProcesses, HDProfileId.CustomPostProcessBeforeTAA);
 
                 // Temporal anti-aliasing goes first
@@ -1131,6 +1133,8 @@ namespace UnityEngine.Rendering.HighDefinition
 */
                 hdCamera.resetPostProcessingHistory = false;
             }
+
+            hdCamera.Schedule = HDCamera.CameraSchedule.AfterPostProcessing;
 
             // Contrast Adaptive Sharpen Upscaling, which only works after post effects
             if (DynamicResolutionHandler.instance.schedulePolicy == DynamicResSchedulePolicy.AfterPost)
